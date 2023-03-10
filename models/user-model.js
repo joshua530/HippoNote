65b1a5b19e367cd7e0c29c2002859596e040f6ff
joshua530 <joshuaomari5@gmail.com>
2023-03-10 10:34:07 +0300

    implement getNotes method

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
+import UserNote from "./usernote-model";
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    password: String,
-    email: String,
-    profilePicture: String,
-    dateCreated: Date,
-});
+const userSchema = new Schema(
+    {
+        password: String,
+        email: String,
+        profilePicture: String,
+        dateCreated: Date,
+    },
+    {
+        methods: {
+            getNotes() {
+                return UserNote.find({ id: this.id });
+            },
+        },
+    }
+);
 
-export default mongoose.Model("User", userSchema);
+const User = mongoose.Model("User", userSchema);
+export default User;
