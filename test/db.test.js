b189b2a0fe12a6961c32516e2a03af462a8b71a4
joshua530 <joshuaomari5@gmail.com>
2023-03-11 10:23:58 +0300

    add database tests

---
+++ test/db.test.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+const connection = require("../db");
+const { expect } = require("chai");
+
+describe("Db connector", function () {
+    afterEach(async function () {
+        mongoose.connection.close();
+    });
+    it("Should connect to local database when DB_URI isn't provided", async function () {
+        process.env.DB_URI = null;
+        process.env["PORT"] = 27017;
+        process.env["HOST"] = "127.0.0.1";
+        process.env["DB"] = "hippo_note";
+        await connection();
+        expect(mongoose.connection.readyState).to.be.equal(1);
+    });
+    it("Should connect to database using DB_URI when provided", async function () {
+        process.env.DB_URI = "mongodb://127.0.0.1:27017/hippo_note";
+        process.env["PORT"] = null;
+        process.env["HOST"] = null;
+        process.env["DB"] = null;
+        await connection();
+        expect(mongoose.connection.readyState).to.be.equal(1);
+    });
+});
