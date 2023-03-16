59119ea011fa5852161472dc27e337e8efed844b
joshua530 <joshuaomari5@gmail.com>
2023-03-16 07:25:00 +0300

    set db uri env var

--- test/models/user-model.test.js
+++ test/models/user-model.test.js
@@ -8,9 +8,11 @@ const UserNote = require("../../models/usernote-model");
 describe("User Model", function () {
     before(async function () {
         await connection();
+        process.env.DB_URI = "hippo_note_test";
     });
     after(function () {
         mongoose.connection.close();
+        process.env.DB_URI = "";
     });
     it("Returns associated user notes", async function () {
         const user = await User.create({
@@ -21,6 +23,7 @@ describe("User Model", function () {
         if (!user.id) expect(1).to.be.equal(0, "User creation failed");
         const note = await Note.create({
             content: "note",
+            title: "title",
         });
         if (!note.id) expect(1).to.be.equal(0, "Note creation failed");
         const userNote = await UserNote.create({
