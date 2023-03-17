3fd200541408e9d02a5aad99602b6805e4cb99ad
joshua530 <joshuaomari5@gmail.com>
2023-03-17 10:18:23 +0300

    add user credentials validation logic

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const User = require("../models/user-model");
 const router = express.Router();
+const { verifyPassword } = require("../utils");
 
 /**
  * route: /
@@ -13,7 +15,29 @@ router
     .get(function (req, res) {
         res.render("login.html", { title: "login page" });
     })
-    .post(function (req, res) {});
+    .post(async function (req, res) {
+        const username = req.body.username;
+        const password = req.body.password;
+        if (!username || !password) {
+            res.status(400);
+            res.render("login.html", {
+                title: "login page",
+                message: "username and password cannot be empty",
+            });
+            return;
+        }
+        const user = await User.findOne({ username: username }).exec();
+        console.log(user);
+        if (!user || !verifyPassword(password, user.password)) {
+            res.status(400);
+            res.render("login.html", {
+                title: "login page",
+                message: "invalid username or password",
+            });
+            return;
+        }
+        res.redirect("/login");
+    });
 
 router
     .route("/sign-up")
