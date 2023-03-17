1c736e70715560e5154b8d6aefd235167d908465
joshua530 <joshuaomari5@gmail.com>
2023-03-17 10:05:07 +0300

    add more login tests

--- test/routes/routes.test.js
+++ test/routes/routes.test.js
@@ -1,33 +1,99 @@
 const { expect } = require("chai");
+const mongoose = require("mongoose");
 const request = require("request");
+const connection = require("../../db");
+const User = require("../../models/user-model");
+const { hashPassword } = require("../../utils");
 
 const port = process.env.PORT || 5000;
+const host = "127.0.0.1";
 describe("Index routes", function () {
     describe("Home page", function () {
         it("Renders correctly", function () {
-            request.get(`http://127.0.0.1:${port}/`, (err, res, body) => {
+            request.get(`http://${host}:${port}/`, (err, res, body) => {
                 expect(body).to.contain("home page");
                 expect(body).to.contain("Hippo note");
             });
         });
     });
     describe("Login page", function () {
+        before(async function () {
+            process.env["DB_URI"] = "mongodb://127.0.0.1:27017/hippo_note_test";
+            await connection();
+            await User.create({
+                username: "abc",
+                password: hashPassword("abc"),
+                email: "a@email.com",
+                profilePicture: "/a.jpg",
+            });
+        });
+        after(async function () {
+            await mongoose.connection.close();
+        });
         it("Renders correctly", function () {
-            request.get(`http://127.0.0.1:${port}/login`, (err, res, body) => {
+            request.get(`http://${host}:${port}/login`, (err, res, body) => {
                 expect(body).to.contain("login page");
                 expect(body).to.contain("Login");
             });
         });
+        it("Returns error message on missing username", function () {
+            request.post(
+                {
+                    url: `http://${host}:${port}/login`,
+                    form: { password: "ab" },
+                },
+                (err, res, body) => {
+                    expect(res.statusCode).to.be.equal(400);
+                    expect(body).to.contain(
+                        "username and password cannot be empty"
+                    );
+                }
+            );
+        });
+        it("Returns error message on missing password", function () {
+            request.post(
+                {
+                    url: `http://${host}:${port}/login`,
+                    form: { password: "ab" },
+                },
+                (err, res, body) => {
+                    expect(res.statusCode).to.be.equal(400);
+                    expect(body).to.contain(
+                        "username and password cannot be empty"
+                    );
+                }
+            );
+        });
+        it("Returns error message for wrong credentials", function () {
+            request.post(
+                {
+                    url: `http://${host}:${port}/login`,
+                    form: { username: "abcde", password: "ab" },
+                },
+                (err, res, body) => {
+                    expect(res.statusCode).to.be.equal(400);
+                    expect(body).to.contain("invalid username or password");
+                }
+            );
+        });
+        it("Returns 200 ok for correct credentials", async function () {
+            request.post(
+                {
+                    url: `http://${host}:${port}/login`,
+                    form: { username: "abc", password: "abc" },
+                },
+                (err, res, body) => {
+                    expect(res.statusCode).to.be.equal(302);
+                }
+            );
+        });
     });
     describe("Sign up page", function () {
         it("Renders correctly", function () {
-            request.get(
-                `http://127.0.0.1:${port}/sign-up`,
-                (err, res, body) => {
-                    expect(body).to.contain("sign up");
-                    expect(body).to.contain("Create an account");
-                }
-            );
+            request.get(`http://${host}:${port}/sign-up`, (err, res, body) => {
+                expect(body).to.contain("sign up");
+                expect(body).to.contain("Create an account");
+            });
         });
     });
 });
