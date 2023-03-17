ff1a41a0fbf3e16015571272b5a49826fb4257b3
joshua530 <joshuaomari5@gmail.com>
2023-03-17 07:13:38 +0300

    add static root routes

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,26 @@ const router = express.Router();
 /**
  * route: /
  */
-router.get("/", function (req, res) {});
+router.get("/", function (req, res) {
+    res.render("index.html", { title: "home page" });
+});
 
 router
     .route("/login")
-    .get(function (req, res) {})
+    .get(function (req, res) {
+        res.render("login.html", { title: "login page" });
+    })
     .post(function (req, res) {});
 
 router
     .route("/sign-up")
-    .get(function (req, res) {})
+    .get(function (req, res) {
+        res.render("sign-up.html", { title: "sign up" });
+    })
     .post(function (req, res) {});
 
-router.get("/dashboard", function (req, res) {});
+router.get("/dashboard", function (req, res) {
+    res.render("dashboard.html", { title: "dashboard" });
+});
 
 module.exports = router;
