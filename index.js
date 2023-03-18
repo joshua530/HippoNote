ef8eff6247edcae45f62eb813fee901c4af8c60a
joshua530 <joshuaomari5@gmail.com>
2023-03-18 09:30:38 +0300

    add cookie parser middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { addSecretToEnv } = require("./utils");
 const app = express();
 const twig = require("twig");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const connection = require("./db");
 
 addSecretToEnv();
@@ -12,6 +13,7 @@ app.set("views", "./templates");
 app.set("view engine", "twig");
 app.engine("html", twig.__express);
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/static", express.static("public"));
 app.use("/", require("./routes"));
