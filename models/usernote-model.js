56d1b9356b0efe537ccd4e5c118e6ee18f666e5e
joshua530 <joshuaomari5@gmail.com>
2023-03-18 11:37:56 +0300

    make userId and noteId required

--- models/usernote-model.js
+++ models/usernote-model.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userNoteSchema = new Schema({
-    userId: mongoose.Types.ObjectId,
-    noteId: mongoose.Types.ObjectId,
+    userId: { type: mongoose.Types.ObjectId, required: true },
+    noteId: { type: mongoose.Types.ObjectId, required: true },
 });
 
 const UserNote = mongoose.model("UserNote", userNoteSchema);
