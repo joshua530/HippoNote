56ce7aaa75d8908106286a8034a4689566e361dd
joshua530 <joshuaomari5@gmail.com>
2023-03-18 10:17:34 +0300

    add image url generation

--- utils.js
+++ utils.js
@@ -49,9 +49,22 @@ function addSecretToEnv(basePath = "") {
     }
 }
 
+/** more utils */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateImageUri() {
+    let uri = `/static/img/profile/${getRandomInt(1, 10)}.png`;
+    return uri;
+}
+
 module.exports = {
     generateSecretKey,
     addSecretToEnv,
     hashPassword,
     verifyPassword,
+    generateImageUri,
 };
