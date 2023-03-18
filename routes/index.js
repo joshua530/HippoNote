bf3eafc28d0fa62629242df48674732f10974f36
joshua530 <joshuaomari5@gmail.com>
2023-03-18 08:54:44 +0300

    implement sign up logic

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/user-model");
 const router = express.Router();
-const { verifyPassword } = require("../utils");
+const { verifyPassword, generateImageUri } = require("../utils");
 
 /**
  * route: /
@@ -27,7 +27,7 @@ router
             return;
         }
         const user = await User.findOne({ username: username }).exec();
-        console.log(user);
+
         if (!user || !verifyPassword(password, user.password)) {
             res.status(400);
             res.render("login.html", {
@@ -44,7 +44,106 @@ router
     .get(function (req, res) {
         res.render("sign-up.html", { title: "sign up" });
     })
-    .post(function (req, res) {});
+    .post(async function (req, res) {
+        let { username, password, email, password2 } = req.body;
+        username = username.trim();
+        password = password.trim();
+        email = email.trim();
+        password2 = password2.trim();
+        if (!username || !email || !password || !password2) {
+            res.status(400);
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "fill in all details",
+                    type: "danger",
+                },
+                username,
+                email,
+            });
+            return;
+        }
+        if (password.length < 8) {
+            res.status(400);
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "password should be at least 8 characters in length",
+                    type: "danger",
+                },
+                username,
+                email,
+            });
+            return;
+        }
+        if (password !== password2) {
+            res.status(400);
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "the two passwords must match",
+                    type: "danger",
+                },
+                username,
+                email,
+            });
+            return;
+        }
+        let existing = await User.findOne({ username });
+        if (existing) {
+            res.status(400);
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "that username is already taken",
+                    type: "danger",
+                },
+                username,
+                email,
+            });
+            return;
+        }
+        existing = await User.findOne({ email });
+        if (existing) {
+            res.status(400);
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "that email is already taken",
+                    type: "danger",
+                },
+                username,
+                email,
+            });
+            return;
+        }
+        let user = await User.create({
+            username,
+            password,
+            email,
+            profilePicture: generateImageUri(),
+        });
+        if (user) {
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "account created successfully, you can now log in with your credentials",
+                    type: "success",
+                },
+            });
+        } else {
+            res.status(400);
+            res.render("sign-up.html", {
+                title: "sign up",
+                message: {
+                    text: "error occured while creating user, please try again",
+                    type: "danger",
+                },
+                username,
+                email,
+            });
+        }
+    });
 
 router.get("/dashboard", function (req, res) {
     res.render("dashboard.html", { title: "dashboard" });
