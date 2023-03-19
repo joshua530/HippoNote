bc6af002b13ed3f401c942417438d3c44489277a
joshua530 <joshuaomari5@gmail.com>
2023-03-19 08:22:01 +0300

    add 404 capture all view

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ app.use("/", require("./routes"));
 app.use("/notes", require("./routes/notes"));
 app.use("/account", require("./routes/account"));
 app.use("*", function (req, res) {
-    res.set("Content-Type", "application/json");
-    res.send({ error: "404 not found" });
+    res.render("404.html");
 });
 
 const port = process.env.PORT || 5000;
