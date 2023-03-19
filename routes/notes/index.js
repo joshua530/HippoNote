e23ae3f6f5f395b6dba13562706bf0fc6bf3a90c
joshua530 <joshuaomari5@gmail.com>
2023-03-19 06:04:41 +0300

    implement note creation

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { loggedIn, getUserIdFromCookie } = require("../../utils");
 const router = express.Router();
+const { body } = require("express-validator");
+const sanitizeHtml = require("sanitize-html");
+const Note = require("../../models/note-model");
+const UserNote = require("../../models/usernote-model");
 
 /**
  * route: /notes
@@ -9,9 +14,67 @@ router.get("/", function (req, res) {});
 router.get("/search", function (req, res) {});
 
 router
-    .route("/:id")
-    .get(function (req, res) {})
-    .post(function (req, res) {});
+    .route("/new")
+    .get(function (req, res) {
+        res.render("create-note.html", { title: "create note" });
+    })
+    .post(
+        body("text").not().isEmpty().trim(),
+        body("title").not().isEmpty().trim().escape(),
+        async function (req, res) {
+            if (!loggedIn(req)) {
+                res.redirect("/login");
+                return;
+            }
+            // sanitize
+            const userId = getUserIdFromCookie(req);
+            const cleanedText = sanitizeHtml(req.body.text, {
+                allowedTags: ["img"],
+                allowedAttributes: {
+                    img: [
+                        "src",
+                        "srcset",
+                        "alt",
+                        "title",
+                        "width",
+                        "height",
+                        "loading",
+                    ],
+                },
+            });
+
+            const note = await Note.create({
+                title: req.body.title,
+                content: cleanedText,
+            });
+            if (!note) {
+                res.render("create-note.html", {
+                    title: "create note",
+                    text: req.body.text,
+                    message:
+                        "could not create note at this time, please try again later",
+                });
+                return;
+            }
+
+            const userNote = await UserNote.create({
+                userId,
+                noteId: note.id,
+            });
+            if (!userNote) {
+                await Note.findOneAndDelete({ _id: note.id });
+                res.render("create-note.html", {
+                    title: "create note",
+                    text: req.body.text,
+                    message:
+                        "could not create note at this time, please try again later",
+                });
+                return;
+            }
+
+            res.redirect("/dashboard");
+        }
+    );
 
 router
     .route("/new")
