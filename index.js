89b9e8529efae91653e5fbb10311d1887fbdb2fc
joshua530 <joshuaomari5@gmail.com>
2023-03-20 08:46:24 +0300

    add header middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const connection = require("./db");
 const authenticate = require("./middleware/auth");
+const headers = require("./middleware/headers");
 
 addSecretToEnv();
 connection(true);
@@ -28,6 +29,7 @@ app.use("/robots.txt", function (req, res) {
 app.use("/", require("./routes"));
 app.use("/notes", require("./routes/notes"));
 app.use("/account", require("./routes/account"));
+app.use("*", headers);
 app.use("*", function (req, res) {
     res.render("404.html");
 });
