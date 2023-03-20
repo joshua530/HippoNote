6a85de639521e266ab24ed459c2a10155541ba25
joshua530 <joshuaomari5@gmail.com>
2023-03-20 05:24:14 +0300

    add profile page

--- routes/account/index.js
+++ routes/account/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const User = require("../../models/user-model");
+const UserNote = require("../../models/usernote-model");
+const { getUserIdFromCookie } = require("../../utils");
 const router = express.Router();
 
 /**
  * route: /account
  */
-router.get("/", function (req, res) {});
+router.get("/", async function (req, res) {
+    const userId = getUserIdFromCookie(req);
+    let user;
+    try {
+        user = await User.findOne({ _id: userId });
+    } catch (e) {
+        res.redirect("/500");
+        return;
+    }
+    if (!user) {
+        res.redirect("/404");
+        return;
+    }
+    let numNotes = await UserNote.count({ userId });
+    res.render("profile.html", { title: "profile", user, numNotes });
+});
 
 router
     .route("/update")
