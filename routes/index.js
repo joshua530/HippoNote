499c7831f29d38b52104e6268e4c60c08afa4caf
joshua530 <joshuaomari5@gmail.com>
2023-03-20 06:59:15 +0300

    update error handling

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const User = require("../models/user-model");
 const router = express.Router();
-const {
-    verifyPassword,
-    generateImageUri,
-    hashPassword,
-    loggedIn,
-} = require("../utils");
+const { verifyPassword, generateImageUri, hashPassword } = require("../utils");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 
 /**
  * route: /
@@ -33,148 +29,130 @@ router
     .get(function (req, res) {
         res.render("login.html", { title: "login page" });
     })
-    .post(async function (req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
-        if (!username || !password) {
-            res.status(400);
-            res.render("login.html", {
-                title: "login page",
-                message: "username and password cannot be empty",
+    .post(
+        body("password", "invalid credentials").not().isEmpty().trim().escape(),
+        body("username", "invalid credentials").not().isEmpty().trim().escape(),
+        async function (req, res) {
+            const errors = validationResult(req).array();
+            if (errors.length > 0) {
+                res.render("create-note.html", {
+                    errors,
+                    username: req.body.username,
+                    password: req.body.password,
+                });
+                return;
+            }
+            const { username, password } = req.body;
+            const user = await User.findOne({ username: username }).exec();
+            if (!user || !verifyPassword(password, user.password)) {
+                res.status(400);
+                res.render("login.html", {
+                    title: "login page",
+                    errors: [{ msg: "invalid credentials" }],
+                });
+                return;
+            }
+            const token = jwt.sign({ id: user.id }, process.env.SECRET, {
+                expiresIn: "10d",
             });
-            return;
-        }
-        const user = await User.findOne({ username: username }).exec();
-        if (!user || !verifyPassword(password, user.password)) {
-            res.status(400);
-            res.render("login.html", {
-                title: "login page",
-                message: "invalid username or password",
-            });
-            return;
-        }
-        const token = jwt.sign({ id: user.id }, process.env.SECRET, {
-            expiresIn: "10d",
-        });
 
-        res.cookie("session", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: true,
-            expires: new Date(Date.now() + 60000 * 60 * 24 * 10),
-        });
-        res.redirect("/dashboard");
-    });
+            res.cookie("session", token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: true,
+                expires: new Date(Date.now() + 60000 * 60 * 24 * 10),
+            });
+            res.redirect("/dashboard");
+        }
+    );
 
 router
     .route("/sign-up")
     .get(function (req, res) {
-        if (loggedIn(req)) {
-            res.redirect("/dashboard");
-            return;
-        }
         res.render("sign-up.html", { title: "sign up" });
     })
-    .post(async function (req, res) {
-        let { username, password, email, password2 } = req.body;
-        username = username.trim();
-        password = password.trim();
-        email = email.trim();
-        password2 = password2.trim();
-        if (!username || !email || !password || !password2) {
-            res.status(400);
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "fill in all details",
-                    type: "danger",
-                },
+    .post(
+        body("username", "invalid username").not().isEmpty().trim().escape(),
+        body("email", "invalid email").isEmail(),
+        body("password", "invalid password")
+            .not()
+            .isEmpty()
+            .trim()
+            .escape()
+            .isLength({ min: 8 })
+            .withMessage("password should be 8 characters or more"),
+        body("password2", "passwords do not match").custom(
+            (value, { req }) => value === req.body.password
+        ),
+        async function (req, res) {
+            let { username, password, email } = req.body;
+            const errors = validationResult(req).array();
+            if (errors.length > 0) {
+                res.render("create-note.html", {
+                    errors,
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    password2: req.body.password2,
+                });
+                return;
+            }
+            let existing = await User.findOne({ username });
+            if (existing) {
+                res.status(400);
+                res.render("sign-up.html", {
+                    title: "sign up",
+                    message: {
+                        text: "that username is already taken",
+                        type: "danger",
+                    },
+                    username,
+                    email,
+                });
+                return;
+            }
+            existing = await User.findOne({ email });
+            if (existing) {
+                res.status(400);
+                res.render("sign-up.html", {
+                    title: "sign up",
+                    message: {
+                        text: "that email is already taken",
+                        type: "danger",
+                    },
+                    username,
+                    email,
+                });
+                return;
+            }
+            let user = await User.create({
                 username,
+                password: hashPassword(password),
                 email,
+                profilePicture: generateImageUri(),
             });
-            return;
+            if (user) {
+                res.render("sign-up.html", {
+                    title: "sign up",
+                    message: {
+                        text: "account created successfully, you can now log in with your credentials",
+                        type: "success",
+                    },
+                });
+            } else {
+                res.status(400);
+                res.render("sign-up.html", {
+                    title: "sign up",
+                    message: {
+                        text: "error occured while creating user, please try again later",
+                        type: "danger",
+                    },
+                    username,
+                    email,
+                });
+            }
         }
-        if (password.length < 8) {
-            res.status(400);
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "password should be at least 8 characters in length",
-                    type: "danger",
-                },
-                username,
-                email,
-            });
-            return;
-        }
-        if (password !== password2) {
-            res.status(400);
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "the two passwords must match",
-                    type: "danger",
-                },
-                username,
-                email,
-            });
-            return;
-        }
-        let existing = await User.findOne({ username });
-        if (existing) {
-            res.status(400);
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "that username is already taken",
-                    type: "danger",
-                },
-                username,
-                email,
-            });
-            return;
-        }
-        existing = await User.findOne({ email });
-        if (existing) {
-            res.status(400);
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "that email is already taken",
-                    type: "danger",
-                },
-                username,
-                email,
-            });
-            return;
-        }
-        let user = await User.create({
-            username,
-            password: hashPassword(password),
-            email,
-            profilePicture: generateImageUri(),
-        });
-        if (user) {
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "account created successfully, you can now log in with your credentials",
-                    type: "success",
-                },
-            });
-        } else {
-            res.status(400);
-            res.render("sign-up.html", {
-                title: "sign up",
-                message: {
-                    text: "error occured while creating user, please try again",
-                    type: "danger",
-                },
-                username,
-                email,
-            });
-        }
-    });
+    );
 
 router.get("/dashboard", async function (req, res) {
     // get cookie
