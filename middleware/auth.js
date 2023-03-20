f25a7493cccf2ad9111302c8a15e2724f12243cd
joshua530 <joshuaomari5@gmail.com>
2023-03-20 07:36:08 +0300

    prevent index route redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function authenticate(req, res, next) {
     let cookie = req.cookies.session;
-
+    req.authenticated = false;
     try {
         jwt.verify(cookie, process.env.SECRET);
         if (req.baseUrl === "/sign-up" || req.baseUrl === "/login") {
@@ -10,13 +10,17 @@ function authenticate(req, res, next) {
             return;
         }
     } catch (e) {
-        if (req.baseUrl !== "/login" && req.baseUrl !== "/sign-up") {
+        if (
+            req.baseUrl !== "/login" &&
+            req.baseUrl !== "/sign-up" &&
+            req.baseUrl !== "/"
+        ) {
             res.clearCookie("session");
             res.redirect("/login");
             return;
         }
     }
-
+    req.authenticated = true;
     next();
 }
 
