5c7cb56e7584bfdd1f8e8a1e70acf26807674980
joshua530 <joshuaomari5@gmail.com>
2023-03-20 06:59:37 +0300

    correct infinite redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,22 +2,21 @@ const jwt = require("jsonwebtoken");
 
 function authenticate(req, res, next) {
     let cookie = req.cookies.session;
-    if (!cookie) {
-        res.redirect("/login");
-        return;
-    }
 
     try {
         jwt.verify(cookie, process.env.SECRET);
+        if (req.baseUrl === "/sign-up" || req.baseUrl === "/login") {
+            res.redirect("/dashboard");
+            return;
+        }
     } catch (e) {
-        res.clearCookie("session");
-        res.redirect("/login");
-        return;
+        if (req.baseUrl !== "/login" && req.baseUrl !== "/sign-up") {
+            res.clearCookie("session");
+            res.redirect("/login");
+            return;
+        }
     }
-    if (req.baseUrl === "/login" || req.baseUrl === "/sign-up") {
-        res.redirect("/dashboard");
-        return;
-    }
+
     next();
 }
 
