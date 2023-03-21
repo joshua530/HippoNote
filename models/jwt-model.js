2432c6503407c3e6e0b69a6fb90be4d873cd6a74
joshua530 <joshuaomari5@gmail.com>
2023-03-21 06:45:45 +0300

    set jwt expiry time

--- models/jwt-model.js
+++ models/jwt-model.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const jwtSchema = new Schema({
     token: { type: String, required: true },
     valid: { type: Boolean, default: false },
+    createdAt: { type: Date, expires: 3600 * 24 * 10 },
 });
 
 const Jwt = mongoose.model("Jwt", jwtSchema);
