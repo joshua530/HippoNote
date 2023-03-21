a9e819c683dc0e803b19382b94ec05b1a70f337f
joshua530 <joshuaomari5@gmail.com>
2023-03-21 05:18:45 +0300

    add jwt validity check

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,16 @@
 const jwt = require("jsonwebtoken");
+const Jwt = require("../models/jwt-model");
 
-function authenticate(req, res, next) {
+async function authenticate(req, res, next) {
     let cookie = req.cookies.session;
     req.authenticated = false;
     try {
-        jwt.verify(cookie, process.env.SECRET);
+        const parsed = jwt.verify(cookie, process.env.SECRET);
+        const data = await Jwt.findOne({ token: parsed.jwt_id });
+        // invalid token
+        if (!data || !data.valid) {
+            throw new Exception();
+        }
         if (req.baseUrl === "/sign-up" || req.baseUrl === "/login") {
             res.redirect("/dashboard");
             return;
