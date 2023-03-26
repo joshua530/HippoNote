06340289db625d7722d65b1d8fe5c3b51cea432b
joshua530 <joshuaomari5@gmail.com>
2023-03-26 11:23:53 +0300

    add unsafe-eval to csp

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,7 +1,7 @@
 function headers(req, res, next) {
     res.header(
         "Content-Security-Policy",
-        "default-src 'self'; script-src 'self' 'unsafe-inline' cdn.ckeditor.com *.cloudflare.com *.jsdelivr.net *.fontawesome.com; style-src 'self' 'unsafe-inline' *.cloudflare.com *.fontawesome.com fonts.googleapis.com unsafe-inline; img-src *; font-src *; media-src *; object-src 'none'; frame-src 'none'; base-uri 'self'"
+        "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.ckeditor.com *.cloudflare.com *.jsdelivr.net *.fontawesome.com; style-src 'self' 'unsafe-inline' *.cloudflare.com *.fontawesome.com fonts.googleapis.com unsafe-inline; img-src *; font-src *; media-src *; object-src 'none'; frame-src 'none'; base-uri 'self'"
     );
     next();
 }
